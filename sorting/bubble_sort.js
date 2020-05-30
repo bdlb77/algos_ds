b1d7d131f2417208b1de652bca155b66e34efe70
bryan leighton <bryan.leigh77@gmail.com>
2020-05-29 22:36:08 -0700

    bubble sort

---
+++ sorting/bubble_sort.js
@@ -0,0 +1,23 @@
+const arr = [8,7,6,5,4,9,2,1,3]
+
+// bubble sort.. take 1 <= i <= i - 1..
+// so always comparing curr el and left.. moving across from i..
+
+const bubbleSort = arr => {
+  let swapped = false;
+
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    // until j - i - 1.. (So if 1 el is sorted.. it wil go until 1 pos less.);
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      // swap el if arr[j] > arr[j + 1]
+      if(arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+  if (!swapped) return;
+  }
+}
+bubbleSort(arr)
+console.log(arr)
